1f6bc33dc43168adedb00963fe777b41b112db75
AnishRane13 <anishrane178@gmail.com>
2024-04-18 19:30:08 +0530

    3rd Project done

--- bmiCalculator/chaiaurcode.js
+++ bmiCalculator/chaiaurcode.js
@@ -1,19 +1,26 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    
-   const height =  parseInt(document.querySelector('#height').value)
-   const weight =  parseInt(document.querySelector('#weight').value)
-   const results = document.querySelector('#results')
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-   if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = "Please give a valid height"
-   }
-   else if (weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = "Please give a valid height"
-   }else{
-    results.innerHTML =  (weight/ ((height*2)/10000)).toFixed(2)
-   }
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
 
-})
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = "Please give a valid height";
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = "Please give a valid weight";
+  } else {
+    const bmi = (weight / ((height * 2) / 10000)).toFixed(2);
+    //    results.innerHTML = bmi;
+
+    if (bmi < 18.6) {
+        results.innerHTML = `Your BMI is ${bmi} and you are under weight`
+    } else if(bmi > 18.6 && bmi < 24.9) {
+        results.innerHTML = `Your BMI is ${bmi} and you are healthy`
+    }else{
+        results.innerHTML = `Your BMI is ${bmi} and you are over weight`
+    }
+  }
+});
